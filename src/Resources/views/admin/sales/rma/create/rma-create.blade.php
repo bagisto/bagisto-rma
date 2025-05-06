918d4437d4ca49d65ac2af247fd8c5f9fa84a606
Paweł <shacky7@gmail.com>
2025-05-06 18:01:52 +0200

    Update rma-create.blade.php

--- src/Resources/views/admin/sales/rma/create/rma-create.blade.php
+++ src/Resources/views/admin/sales/rma/create/rma-create.blade.php
@@ -411,7 +411,7 @@ $customAttributes = app('Webkul\RMA\Repositories\RmaCustomFieldRepository')->wit
                                             ::name="'resolution_type[' + getProductId(product) + ']'" 
                                             rules="required"
                                             v-model="resolutionType[getProductId(product)]"
-                                            @change="getResolutionReason(getProductId(product))"
+                                            @change="$nextTick(() => getResolutionReason(getProductId(product)))"
                                             :label="trans('rma::app.admin.configuration.index.sales.rma.resolution-type')"
                                         >
                                             <option value="">
@@ -458,7 +458,7 @@ $customAttributes = app('Webkul\RMA\Repositories\RmaCustomFieldRepository')->wit
                                             ::name="'resolution_type[' + getProductId(product) + ']'" 
                                             rules="required"
                                             v-model="resolutionType[getProductId(product)]"
-                                            @change="getResolutionReason(getProductId(product))"
+                                            @change="$nextTick(() => getResolutionReason(getProductId(product)))"
                                             :label="trans('rma::app.admin.configuration.index.sales.rma.resolution-type')"
                                         >
                                             <option value="">
